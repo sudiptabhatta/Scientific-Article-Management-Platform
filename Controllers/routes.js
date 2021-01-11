a08d555ac9784c78178ec7056a17b2a42a9419ac
sudiptabhatta <sudiptabhatta05@gmail.com>
2021-01-11 16:48:43 +0600

    login logout module added

--- Controllers/routes.js
+++ Controllers/routes.js
@@ -15,5 +15,46 @@ module.exports = function(app){
         })
     })
    
-  
+    app.get('/login', function(req, res) {
+        res.render('login', {message: req.flash('msg')})
+    })
+    
+    app.post('/auth', function(req, res) {
+        var email = req.body.email
+        var password = req.body.password
+        if (email && password) {
+            connection.query('SELECT * FROM researchers WHERE Email = ? AND Password = ?', [email, password], function(error, results, fields) {
+                if (results.length > 0) {
+                    req.session.loggedin = true
+                    req.session.email = email
+                    res.redirect('/home')
+                } else {
+                    req.flash('msg', 'Incorrect email or password!')
+                    res.redirect('/login')
+                }			
+                res.end()
+            })
+        } else {
+            req.flash('msg', 'Please enter email and passowrd!')
+            res.redirect('/login')
+        }
+    })
+
+    app.get('/home', function(req, res) {
+        if (req.session.loggedin) {
+            res.send('Welcome back, ' + req.session.email + '!')
+        } else {
+            req.flash('msg', 'Please login to view this page!')
+            res.redirect('/login')
+        }
+        res.end()
+    })
+
+    app.get('/logout', function(req, res) {
+        req.session.loggedin = null
+        req.session.email = null
+        req.flash('msg', 'Logged out successfully!')
+        res.redirect('/login')
+    })
+
 }
--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
 const multer = require('multer')
+const session = require('express-session')
+const flash = require('connect-flash')
 const controller = require('./Controllers/routes')
 
 // express app
@@ -15,8 +17,16 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 // middleware
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// use connect-flash to store flash messages in the session
+app.use(flash())
 
 controller(app)
 
