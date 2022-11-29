b99c4cee9a8276452f99bc5ea3b926819d21cfab
sudiptabhatta <sudiptabhatta05@gmail.com>
2022-11-29 23:02:21 +0600

    signup form implementation & email field added

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'users/home.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('home')
+            
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
